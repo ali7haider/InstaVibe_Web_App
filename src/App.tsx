ad3ec1b3956860ad42cb502ed12a77eeb3622d49
Ali Haider <haider556871@gmail.com>
2024-02-19 22:24:47 +0500

    Files and Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Routes,Route } from 'react-router-dom';
 import './globals.css'
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+
 const App = () => {
   return (
-    <div><h1 className="text-3xl font-bold underline">
-    Ali Haider!
-  </h1></div>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* Public Routes */}
+        <Route path='/sign-in' element={<SigninForm />}/>
+        <Route path='/sign-up' element={<SignupForm />}/>
+
+        {/* Private Routes */}
+        <Route index element={<Home />}/>
+
+      </Routes>
+    </main>
   )
 }
 
